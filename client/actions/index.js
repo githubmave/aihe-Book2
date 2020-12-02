28c4b56309d391dbe3a89293440f8509fedbc2f6
Kienan Leung <leung.kienan@gmail.com>
2020-12-03 10:08:03 +1300

    reducer and actions updated

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,31 +1,28 @@
 import { getFruits } from '../apis/fruits'
 
-export const RECIEVE_VIDEOS = 'RECIEVE_VIDEOS'
+export const RECEIVE_VIDEOS = 'RECEIVE_VIDEOS'
 
 export const SET_FRUITS = 'SET_FRUITS'
 
-export const recieveVideos = (videoData) => {
-
-       return {
-         type: RECIEVE_VIDEOS,
-         videoData
-         
-       }
-}
-
-export function setFruits (fruits) {
+export const receiveVideos = (videoData) => {
   return {
-    type: SET_FRUITS,
-    fruits
+    type: RECEIVE_VIDEOS,
+    videoData,
   }
 }
 
-export function fetchFruits () {
-  return dispatch => {
-    return getFruits()
-      .then(fruits => {
-        dispatch(setFruits(fruits))
-        return null
-      })
+export function setFruits(fruits) {
+  return {
+    type: SET_FRUITS,
+    fruits,
+  }
+}
+
+export function fetchFruits() {
+  return (dispatch) => {
+    return getFruits().then((fruits) => {
+      dispatch(setFruits(fruits))
+      return null
+    })
   }
 }
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 //import Nav from './Nav'
 
-import { fetchFruits ,recieveVideos} from '../actions'
+import { fetchFruits, receiveVideos } from '../actions'
 import fetchVideos from '../apis/videos'
 
 export class App extends React.Component {
@@ -11,52 +11,48 @@ export class App extends React.Component {
   // }
 
   state = {
-    videos: []
+    videos: [],
   }
 
-  componentDidMount () {
-   // this.props.dispatch(fetchFruits())
+  componentDidMount() {
+    // this.props.dispatch(fetchFruits())
 
     fetchVideos()
-    //  console.log(res.body)
-      .then (videoData => {
+      //  console.log(res.body)
+      .then((videoData) => {
         console.log(videoData)
-       // this.setState({videos: res})
-        this.props.dispatch(recieveVideos(videoData))
-        
+        // this.setState({videos: res})
+        this.props.dispatch(receiveVideos(videoData))
       })
-       // console.log("what is in state") 
-      .catch( err => console.log(err))
-      
-
+      // console.log("what is in state")
+      .catch((err) => console.log(err))
   }
 
-  render () {
+  render() {
     return (
-      <div className='app'>
+      <div className="app">
         <h1>Fullstack Boilerplate - with Fruits!</h1>
         {/* <ul>
           {this.props.fruits.map(fruit => (
             <li key={fruit}>{fruit}</li>
           ))}
         </ul> */}
-      <div >
-        {/* <Nav /> */}
-      </div>
+        <div>{/* <Nav /> */}</div>
 
         <ul>
-
-            {this.props.videos.map( (video,i) => (<li key = {i}> {video.video_name}  </li>))}
+          {this.props.videos.map((video, i) => (
+            <li key={i}> {video.video_name} </li>
+          ))}
         </ul>
       </div>
     )
   }
 }
 
-function mapStateToProps (globalState) {
+function mapStateToProps(globalState) {
   return {
     //fruits: globalState.fruits,
-    videos: globalState.videos
+    videos: globalState.videos,
   }
 }
 
