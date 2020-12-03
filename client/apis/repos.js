3bf9dc8a29d5a2e01c73b9e4e14774a9b0909ee9
lukin-ackroyd <lukinack@gmail.com>
2020-12-04 11:10:36 +1300

    minor issue with repos api

--- client/apis/repos.js
+++ client/apis/repos.js
@@ -3,7 +3,7 @@ import { getRepos } from '../actions/index'
 
 export function getAllRepos (dispatch) {
   return request
-    .get('/api/v1')
+    .get('/api/v1/repos')
     .then((res) => {
       dispatch(getRepos(res.body))
       return null
