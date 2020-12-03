1de3f561338571fea87589a5eb3068a572f66712
Kienan Leung <leung.kienan@gmail.com>
2020-12-04 10:30:08 +1300

    fixing tests

--- client/apis/videos.test.js
+++ client/apis/videos.test.js
@@ -1,14 +1,32 @@
-import { RECEIVE_VIDEOS } from '../actions'
 import nock from 'nock'
-import { TestScheduler } from 'jest'
+import { fetchVideos } from './'
 
 describe('get all videos', () => {
-  const video = 'new video'
-  const fakeTodo = {
-    id: 1,
-    video_name: 'authentication',
-    video_link: 'https://www.youtube.com/watch?v=j3d5NdG6c6k',
-    video_category: 'auth',
-  }
-  
+  const fakeVideo = [
+    {
+      id: 1,
+      video_name: 'authentication',
+      video_link: 'https://www.youtube.com/watch?v=j3d5NdG6c6k',
+      video_category: 'auth',
+    },
+    {
+      id: 2,
+      video_name: 'intro to react',
+      video_link: 'https://www.youtube.com/watch?v=rt43528rthh',
+      video_category: 'react',
+    },
+  ]
+
+  const scope = nock('http://localhost')
+    .get('/api/v1/videos')
+    .reply(200, fakeVideo)
+
+  test('returns a video from api', () => {
+    expect.assertions(2)
+    return fetchVideos().then((videos) => {
+      expect(videos).toEqual(fakeVideo)
+      expect(scope.isDone()).toBe(true)
+      return null
+    })
+  })
 })
