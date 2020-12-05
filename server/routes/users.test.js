99337021bfcfe81c70917bae33d082bf4f622d8a
Jayden Gray <graydenjay@me.com>
2020-12-06 12:59:24 +1300

    Remove fruit api and start testing user route

---
+++ server/routes/users.test.js
@@ -0,0 +1,25 @@
+const request = require('supertest')
+const {
+  userExists,
+  getUserByName,
+  createUser
+} = require('../db/users')
+const server = require('../server')
+
+const users = [{id: 1, username: 'Test', email: 'test@test.com', password: 'password123' }]
+
+jest.mock('../db/users.js', () => ({
+  getUserByName: jest.fn(() => Promise.resolve(users))
+}))
+
+describe('GET /api/v1', () => {
+  test('get user by name', () => {
+    return request(server)
+      .get('/api/v1')
+      .then(res => {
+        expect(getUserByName).toHaveBeenCalled()
+        expect(res.body).toHaveLength(1)
+        return null
+      })
+  })
+})
