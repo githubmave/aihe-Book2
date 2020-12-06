b867800696ece8e3bd946d2644bbf38dc421ceaa
Jayden Gray <graydenjay@me.com>
2020-12-06 14:29:56 +1300

    Update video api with auth

--- client/apis/videos.js
+++ client/apis/videos.js
@@ -1,26 +1,48 @@
 import request from 'superagent'
+import { getAuthorizationHeader } from 'authenticare/client'
+
+const acceptJsonHeader = { Accept: 'application/json' }
 
 // c
 export function postVideo (title, link) {
-  console.log(title)
-  console.log(link)
   return request
+    .set(acceptJsonHeader)
+    .set(getAuthorizationHeader())
     .post('/api/v1/videos')
     .send({ title, link })
     .then((response) => response.body)
+    .catch(logError)
 }
 // r
 export function fetchVideos () {
-  return request.get('/api/v1/videos').then((res) => {
-    return res.body
-  })
+  return request
+    .set(acceptJsonHeader)
+    .get('/api/v1/videos')
+    .then((res) => {
+      return res.body
+    })
 }
 // u
 export function patchVideo (id, patchInfo) {
   return request
+    .set(acceptJsonHeader)
+    .set(getAuthorizationHeader())
     .patch('/api/v1/videos/' + id)
     .send(patchInfo)
     .then((response) => response.body)
 }
 // d
 export function deleteVideo (id) {}
+
+function logError (err) {
+  if (err.message === 'Forbidden') {
+    throw new Error('Only the user who added the fruit may update and delete it')
+  } else {
+    // eslint-disable-next-line no-console
+    console.error(
+      'Error consuming the API (in client/api.js):',
+      err.message
+    )
+    throw err
+  }
+}
