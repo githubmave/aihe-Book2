5221d0e2e818df03e495a3ea5969ef510945e978
Kienan Leung <leung.kienan@gmail.com>
2020-12-09 14:13:23 +1300

    connection test fixed

--- server/db/videodb.js
+++ server/db/videodb.js
@@ -4,34 +4,21 @@ function findVideo (id, db = connection) {
   return db('videos').where({ id }).select().first()
 }
 
-// c
-// function addVideo (title, link, db = connection) {
 function addVideo (newVideo, db = connection) {
   return db('videos')
     .insert(newVideo)
     .then((ids) => {
       return findVideo(ids[0], db)
-
-      // return db('videoCats')
-      // .insert(newVideo.cat_id)
     })
 }
 
-// r
 function getVideos (db = connection) {
   return db('videos').select()
 }
 
-// u
-function updateVideos () {}
-
-// d
-function deleteVideos () {}
 
 module.exports = {
   findVideo,
   addVideo,
-  getVideos,
-  updateVideos,
-  deleteVideos
+  getVideos
 }
