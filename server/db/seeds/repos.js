6b6b0a54eff88f6f8bf6e96b5cd4d043fde9524b
lukin-ackroyd <lukinack@gmail.com>
2020-12-09 15:15:45 +1300

    removed broken tests

--- server/db/seeds/repos.js
+++ server/db/seeds/repos.js
@@ -18,6 +18,32 @@ exports.seed = function (knex) {
         { id: 12, repo_name: 'TheButton', repo_link: 'https://github.com/aihe-2020/TheButton' },
         { id: 13, repo_name: 'code-from-class', repo_link: 'https://github.com/aihe-2020/code-from-class' },
         { id: 14, repo_name: 'React-App-Project', repo_link: 'https://github.com/aihe-2020/React-App-Project' }
+        { id: 15, repo_name: 'async-redux-stories', repo_link: 'https://github.com/aihe-2020/async-redux-stories' },
+        { id: 16, repo_name: 'taxonomic-routing', repo_link: 'https://github.com/aihe-2020/taxonomic-routing' }
+        { id: 17, repo_name: 'sweet-as-beers', repo_link: 'https://github.com/aihe-2020/sweet-as-beers' },
+        { id: 18, repo_name: 'boilerplate-react-webpack', repo_link: 'https://github.com/aihe-2020/boilerplate-react-webpack' }
+        { id: 19, repo_name: 'react-redux-stories', repo_link: 'https://github.com/aihe-2020/react-redux-stories' },
+        { id: 20, repo_name: 'lightning-talks', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'redux-minimal', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'broken-kaleidoscope', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'donbot', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'surrendedAt13', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'You-Are-Fired', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'meme-generator', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'consuming-external-apis', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'knex-todo-cli', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'Banksies-untiled-2020-project', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'soundboard-senpai', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'cursed-cursor', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'knex-relationships-stories', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'react-form-demo', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'ascii-art-reader', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'totally-legal-pokedex', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'consuming-external-apis ', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'consuming-external-apis ', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'consuming-external-apis ', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'consuming-external-apis ', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
+        { id: 20, repo_name: 'consuming-external-apis ', repo_link: 'https://github.com/aihe-2020/lightning-talks' },
       ])
     })
 }
